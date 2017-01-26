e1a3c61baa83f8ff8285c72eae905f87a45ef3e6
casperjureskog <lcasper@mac.com>
2017-01-26 23:30:47 +0100

    corects less than  error in fizz_buzz_spec.rb

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,10 +1,28 @@
 # lib/fizz_buzz.rb
 
-#def fizz_buzz(number)
+print "Välkomnna till Fizz_Buzz!"
+print "var god skriv in ett nummer som du vill kolla om det är delbart med 3, 5, eller båda."
+
+
+
 def fizz_buzz(number)
-  if number % 3 == 0
-  'fizz'
-else
-  number
-  end
+
+  if  number < 1
+    'wrong input'
+elsif number % 15 == 0
+    'fizzbuzz'
+elsif number % 5 == 0
+    'buzz'
+elsif  number % 3 == 0
+    'fizz'
+
+ else
+number
+
+
+end
+end
+
+def has_zero_remainder?(number, divider)
+  number % divider == 0
 end
--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -2,14 +2,28 @@ require './lib/fizz_buzz'
 
 describe 'fizz_buzz' do
 
-  # spec/fizz_buzz_spec.rb
+
+
+  #it 'returns 0 if number is 0' do
+  #  expect(fizz_buzz(0)).to eq 0
+  #end
+
+  it 'returns less than number 1' do
+      expect(fizz_buzz(1)).not_to eq 'wrong input'
+  end
 
   it 'returns 1 if number is 1' do
       expect(fizz_buzz(1)).to eq 1
   end
 
-
   it "returns 'fizz' if number is divisible by 3" do
     expect(fizz_buzz(3)).to eq 'fizz'
   end
+  it "returns 'buzz' if number is divisible by 5" do
+    expect(fizz_buzz(5)).to eq 'buzz'
+  end
+  it "returns 'fizzbuzz' if number is divisible by 15" do
+    expect(fizz_buzz(15)).to eq 'fizzbuzz'
+  end
+
 end
